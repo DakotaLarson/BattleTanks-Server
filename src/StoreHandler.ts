873229b71f71c21f18b47aeca19ec6a25f4b6f3a
Dakota Larson <dakotalarson1996@gmail.com>
2019-07-15 07:52:23 -0500

    color list fix

--- src/StoreHandler.ts
+++ src/StoreHandler.ts
@@ -48,6 +48,9 @@ export default class StoreHandler {
         "Light Gray",
         "Charcoal",
         "Gold",
+        "Sand",
+        "Brown",
+        "Dark Gray",
     ];
 
     private databaseHandler: StoreDatabaseHandler;
